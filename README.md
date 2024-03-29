e54dc73d19b176c1ba3c841a924ee19e91c332af
Aryan soni <aryansoni1973@gmail.com>
2024-03-29 06:09:52 +0530

    Implemented Simple Calculator Project using HTML, CSS, and JavaScript
    This project is a Simple Calculator built following The Odin Project's fullstack development curriculum. It features a user-friendly interface with the following components:
    
    Buttons: Includes buttons for digits, arithmetic operations (+, -, *, /, %), and special functions like clear and backspace.
    Functionality: Implemented using JavaScript to handle button clicks and keyboard inputs, updating the display and performing calculations accordingly.
    evaluateEquation Function: Accurately evaluates mathematical expressions, supporting basic arithmetic operations.
    CSS Styling: Enhances the visual appeal with clean and modern design elements.
    Overall, this project provides a functional calculator tool that can be further extended with additional features and optimizations.
    -----
    #HTML #CSS #JavaScript #Calculator #TheOdinProject

---
+++ README.md
@@ -0,0 +1,81 @@
+# Calculator 🧠 𝞹
+
+![cover](./images/cover.png)
+
+You can try the calculator live [here]()!
+
+## Description
+
+This Simple Calculator is a JavaScript project developed as part of the Odin Project's JavaScript Foundation course curriculum. It provides basic arithmetic operations such as addition, subtraction, multiplication, and division, allowing users to perform calculations easily. The calculator features a user-friendly interface with on-screen buttons for input and real-time display of input history and calculation results.
+
+
+## Table of Contents
+
+| Section              |
+| --------------------|
+| [Demo](#demo)        |
+| [Features](#features)|
+| [How to Use](#how-to-use) |
+| [Installation](#installation) |
+| [Contributing](#contributing) |
+| [Contact](#contact)  |
+| [Acknowledgements](#acknowledgements) |
+| [License](#license)  |
+
+## Features
+
+- Basic arithmetic operations: addition, subtraction, multiplication, division.
+- Support for the percentage (%) operator.
+- Keyboard support for enhanced user experience.
+- Clear button to reset the calculator.
+- Real-time display of input history and calculation result.
+
+## How to Use
+
+1. Enter numbers using the on-screen keypad or your computer keyboard.
+2. Use the buttons for arithmetic operations (+, -, *, /, %).
+3. Press Enter or click the "=" button to evaluate the expression.
+4. Use the "C" button to clear the input and result.
+5. Enjoy performing calculations effortlessly!
+
+## Installation
+
+There's no need for installation. Simply download or clone the repository to your local machine and open the `index.html` file in a web browser.
+
+```bash
+git clone https://github.com/aryansoni-git/odin-calculator
+```
+## Navigate to the project directory:
+
+```bash
+cd odin-calculator
+```
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature/new-feature`).
+3. Make your changes.
+4. Commit your changes (`git commit -am 'Add new feature'`).
+5. Push to the branch (`git push origin feature/new-feature`).
+6. Create a new Pull Request.
+
+## Contact
+
+If you have any questions or suggestions regarding this project, feel free to contact me:
+
+| Platform | Link |
+| --- | --- |
+| Email | [<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg" width="50" height="50">](mailto:aryansoni.work@gmail.com) |
+| GitHub | [<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" width="50" height="50">](https://github.com/aryansoni-git) |
+| Twitter | [<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/twitter/twitter-original.svg" width="50" height="50">](https://twitter.com/aryansoni_x) |
+| LinkedIn | [<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg" width="50" height="50">](https://www.linkedin.com/in/aryansoni-work) |
+| Instagram | [aryansoni.ig](https://www.instagram.com/aryansoni.ig/) |
+
+## Acknowledgements
+
+- Special thanks to [The Odin Project](https://www.theodinproject.com/) for providing the educational resources and project idea that inspired this Etch-a-Sketch implementation.
+- Thanks to [Google Fonts](https://fonts.google.com/specimen/Montserrat?query=Mon) for providing the Montserrat font, which was used in this project.
+
---
+++ script.js
@@ -0,0 +1,176 @@
+// Function to evaluate a mathematical expression
+const evaluateEquation = (equation) => {
+    // Split the equation into segments (numbers and operators)
+    const segments = equation.split(/([-+*/%])/);
+    let result = parseFloat(segments[0]); // First operand
+
+    // Iterate through the segments
+    for (let i = 1; i < segments.length; i += 2) {
+        const operator = segments[i]; // Operator
+        const nextNumber = parseFloat(segments[i + 1]); // Next operand
+
+        // Perform the appropriate operation based on the operator
+        switch (operator) {
+            case '+': result += nextNumber; break;
+            case '-': result -= nextNumber; break;
+            case '*': result *= nextNumber; break;
+            case '/': result /= nextNumber; break;
+            case '%': result %= nextNumber; break;
+            default:
+                alert('Invalid operator:', operator);
+                return NaN;
+        }
+    }
+    // Limit the result to 8 significant digits
+    if (result.toString().length > 9) {
+        result = parseFloat(result.toPrecision(8));
+    }
+    return Number(result);
+}
+
+// Get the button container element
+const buttonContainer = document.querySelector('#button-container');
+
+// Event listener for button clicks
+buttonContainer.addEventListener('click', (e) => {
+    const resultElement = document.querySelector('#result');
+    const historyElement = document.querySelector('#history');
+    const button = e.target;
+
+    // Switch case to handle different button clicks
+    let equation = '';
+    switch (button.id) {
+        // Digits
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            historyElement.textContent += button.textContent;
+            break;
+        case 'dot':
+            // Add decimal point if not already present
+            if (!historyElement.textContent.includes('.')) {
+                historyElement.textContent += button.textContent;
+            }
+            break;
+        // Operators
+        case '%':
+        case '/':
+        case '*':
+        case '-':
+        case '+':
+            historyElement.textContent += ` ${button.textContent} `;
+            break;
+        // Clear and Evaluate
+        case 'Escape':
+            resultElement.textContent = 0;
+            historyElement.textContent = '';
+            break;
+        case 'Backspace':
+            // Remove the last character from the history
+            let historyText = historyElement.textContent;
+            historyText = historyText.slice(0, -1);
+            historyElement.textContent = historyText;
+            break;
+        case 'Enter':
+            // Evaluate the expression
+            equation = historyElement.textContent;
+            const result = evaluateEquation(equation);
+            resultElement.textContent = result;
+            if (!isNaN(result)) {
+                historyElement.textContent = result;
+            } else {
+                historyElement.textContent = '';
+            }
+            break;
+        default: // This section needs bug fixes and improvements...
+            // Append digit to history if clicked button represents a digit
+            if (!isNaN(parseInt(id))) {
+                historyText += id;
+            }
+            // Append decimal point to history if dot button clicked and history doesn't have one
+            else if (id === 'dot' && !historyText.includes('.')) {
+                historyText += id;
+            }
+            // Handle operators (+, -, *, /, %)
+            else if (/[-+*/%]/.test(id)) {
+                // Add operator to history if last character isn't already an operator
+                if (!/[-+*/%]/.test(historyText.charAt(historyText.length - 1))) {
+                    historyText += ` ${id} `;
+                } else {
+                    // Replace last operator if it's already an operator
+                    historyText = historyText.slice(0, -1) + id + ' ';
+                }
+            }
+            break;
+    }
+});
+
+// Event listener for keyboard inputs
+document.addEventListener('keydown', (e) => {
+    const key = e.key;
+    // Get display elements
+    const resultElement = document.querySelector('#result');
+    const historyElement = document.querySelector('#history');
+    let historyText = historyElement.textContent.trim();
+
+    // Style the button for visual feedback
+    const button = document.getElementById(key);
+    if (button && button.classList.contains('btn')) {
+        button.style.backgroundColor = 'rgb(246 246 246)';
+        button.style.transform = 'scale(1.1)';
+        setTimeout(() => {
+            button.style.transform = 'scale(1)';
+        }, 200)
+        button.addEventListener('transitionend', () => {
+            button.style.backgroundColor = '';
+        });
+    }
+
+    // Switch case to handle keyboard inputs
+    switch (key) {
+        case 'Escape':
+            // Clear the display
+            resultElement.textContent = '0';
+            historyText = '';
+            break;
+        case 'Backspace':
+            // Remove the last character from the history
+            historyText = historyText.slice(0, -1);
+            break;
+        case 'Enter':
+            // Evaluate the expression
+            const equation = historyText;
+            const result = evaluateEquation(equation);
+            if (!isNaN(result)) {
+                resultElement.textContent = result;
+            } else {
+                resultElement.textContent = '';
+            }
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+            // Add operator if the last character is a number
+            if (!isNaN(parseFloat(historyText.charAt(historyText.length - 1)))) {
+                historyText += ` ${key}  `;
+            }
+            break;
+        default:
+            // Add digit or decimal point to the history
+            if (!isNaN(parseFloat(key)) || key === '') {
+                historyText += key;
+            }
+            break;
+    }
+    // Update the history display
+    historyElement.textContent = historyText;
+});
